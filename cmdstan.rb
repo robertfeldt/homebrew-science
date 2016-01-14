4d556db40a18b0b3f529e5411667f0bd6d62c00d
Robert Feldt <robert.feldt@gmail.com>
2016-01-14 15:24:10 +0100

    updated to cmdstan 2.9.0

--- cmdstan.rb
+++ cmdstan.rb
@@ -10,19 +10,19 @@ class Cmdstan < Formula
 
   # tag "math"
 
-  url "https://github.com/stan-dev/cmdstan/releases/download/v2.7.0/cmdstan-2.7.0.tar.gz"
-  sha256 "8f8f119a88327a7cef9829cb593a1d726a65c6bc37cc85ef11f5442add31d9e1"
+  url "https://github.com/stan-dev/cmdstan/releases/download/v2.9.0/cmdstan-2.9.0.tar.gz"
+  sha256 "a9f2858caa5b55576da85ef31b4eae632c97837aa042514242a9aad7ada97121"
 
   depends_on "boost"
   depends_on "eigen"
 
   def install
     system "make", "build"
-    bin.install "bin/print" => "stansummary"
+    bin.install "bin/stansummary"
     bin.install "bin/stanc"
     doc.install "CONTRIBUTING.md", "LICENSE", "README.md", "examples"
-    include.install "stan/src/stan"
-    (include/"stan").install Dir["stan/lib/stan_math_*/stan/*"]
+    include.install "stan_2.9.0/src/stan"
+    (include/"stan").install Dir["stan_2.9.0/lib/stan_math_*/stan/*"]
   end
 
   test do
